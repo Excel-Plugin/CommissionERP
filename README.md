dff075d34bc1d330473ebfe874d61a3fa23bdca3
zsnjuts <1290645535@qq.com>
2018-09-23 19:46:53 +0800

    添加了manageWidget的一些逻辑，可以正常点击了

---
+++ README.md
@@ -0,0 +1,5 @@
+# 注意事项
+更新了资源文件之后需要使用如下命令重新生成相应的py代码文件
+```
+pyrcc5 resources.qrc -o resources.py
+```
--- manage_widget.py
+++ manage_widget.py
@@ -1,7 +1,9 @@
 import sys
 
-from PyQt5.QtWidgets import QWidget, QListWidgetItem, QHBoxLayout, QLabel, QPushButton, QApplication
+from PyQt5 import Qt
+from PyQt5.QtWidgets import QWidget, QListWidgetItem, QHBoxLayout, QLabel, QPushButton, QApplication, QHeaderView
 from PyQt5.uic import loadUi
+import resources
 
 
 class ListWidgetItem(QListWidgetItem):
@@ -20,10 +22,34 @@ class ManageWidget(QWidget):
     def __init__(self):
         super(ManageWidget, self).__init__()
         loadUi('manage_widget.ui', self)
-        self.resultListWidget.addItem(QListWidgetItem("数据源表2018-08"))
-        self.resultListWidget.addItem(QListWidgetItem("数据源表2018-07"))
-        self.resultListWidget.addItem(QListWidgetItem("数据源表2018-06"))
-        self.resultListWidget.addItem(QListWidgetItem("数据源表2018-05"))
+        self.setWindowFlags(Qt.Qt.WindowMinimizeButtonHint | Qt.Qt.WindowCloseButtonHint)
+        self.setFixedSize(self.width(), self.height())
+        self.listTableWidget.horizontalHeader().setDefaultAlignment(Qt.Qt.AlignLeft)
+        self.listTableWidget.horizontalHeader().setDefaultSectionSize(250)
+        self.tableManagePushButton.setChecked(True)
+        self.tableManagePushButton.clicked.connect(self.tableManagePushButtonClickedSlot)
+        self.tableGeneratePushButton.setChecked(False)
+        self.tableGeneratePushButton.clicked.connect(self.tableGeneratePushButtonClickedSlot)
+        self.tableViewPushButton.setChecked(False)
+        self.tableViewPushButton.clicked.connect(self.tableViewPushButtonClickedSlot)
+
+    def tableManagePushButtonClickedSlot(self):
+        self.tableManagePushButton.setChecked(True)
+        self.tableGeneratePushButton.setChecked(False)
+        self.tableViewPushButton.setChecked(False)
+        self.stackedWidget.setCurrentIndex(0)
+
+    def tableGeneratePushButtonClickedSlot(self):
+        self.tableManagePushButton.setChecked(False)
+        self.tableGeneratePushButton.setChecked(True)
+        self.tableViewPushButton.setChecked(False)
+        self.stackedWidget.setCurrentIndex(1)
+
+    def tableViewPushButtonClickedSlot(self):
+        self.tableManagePushButton.setChecked(False)
+        self.tableGeneratePushButton.setChecked(False)
+        self.tableViewPushButton.setChecked(True)
+        self.stackedWidget.setCurrentIndex(2)
 
 
 if __name__ == '__main__':
